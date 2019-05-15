269760a53855afbc57fb5f319b332e19b015ad84
f-shixiong <944406247@qq.com>
2019-05-15 08:48:25 +0800

    1

---
+++ compiler/demo/raojian2.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class raojian2{
+	public static void main(String[] args) {
+		System.out.println(new Date());
+		System.out.println("pirnt time");
+	}
+
+}
--- compiler/test.sh
+++ compiler/test.sh
@@ -1,3 +1,3 @@
 export ROBOVM_HOME='/home/jon-wwr/work/src/github.com/f-shixiong/s-jvm' #修改成lib的路径
 mvn clean compile package -Dmaven.test.skip=true
-java -classpath target/robovm-compiler-1.8.0-jar-with-dependencies.jar org.robovm.compiler.AppCompiler  -classpath ./demo/ raojian
+java -classpath target/robovm-compiler-1.8.0-jar-with-dependencies.jar org.robovm.compiler.AppCompiler  -classpath ./demo/ raojian2
